f19a27e975868c3d858aac6803c78e88f480628a
korawitice <korawit34179@gmail.com>
2020-09-15 05:50:42 +0700

    edit Usertest

--- Test/unit/Usertest.spec.js
+++ Test/unit/Usertest.spec.js
@@ -8,7 +8,7 @@ test('should return undefined error when input correct data', async ({ assert })
     frist_name: "iceiceice",
     last_name:"tototo",
     email:"icehHhHhhah@gmail.com",
-    password: "Gogoza"
+    password: "Gogoza11223"
   })
   assert.isArray(validatedData.error, undefined)
 })
@@ -17,8 +17,8 @@ test('should return error if input incorrect data', async ({ assert }) => {
   const validatedData = await UserValidator({
     frist_name: "korawit",
     last_name:"wilairat",
-    email:"icezaza@gmail.com",
-    password: "11223344"
+    email:"icezaza",
+    password: "1122"
   })
   assert.isArray(validatedData.error)
 })
@@ -27,16 +27,16 @@ test('should return only one error if input incorrect password', async ({ assert
     frist_name: "kokozza",
     last_name:"janama",
     email:"iceyoyo@gmail.com",
-    password: "iceiceza"
+    password: "zaza11223344"
   })
   assert.equal(validatedData.error.length, 3)
 })
-test('should return error if input not unique username', async ({ assert }) => {
+test('should return error if input not unique email', async ({ assert }) => {
   const validatedData = await UserValidator({
     frist_name: "iceiceice",
     last_name:"tototo",
     email:"icerota@gmail.com",
-    password: "112244"
+    password: "112244556677"
   })
   assert.isOk(validatedData.error)
 })
