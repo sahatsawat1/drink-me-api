6523ca3f324ad8d62500860bb75d678f84c09060
Talinee <bieteuy@gmail.com>
2020-09-14 17:19:02 +0700

    booking system

--- app/Controllers/Http/TableController.js
+++ app/Controllers/Http/TableController.js
@@ -1,6 +1,17 @@
 'use strict'
-
+const Database = use("Database");
+const Booking = use('App/Models/Table');
 class TableController {
+    async booking ( { request, response, auth }){
+    const { seat, price , table_id } = request.body
+    const booking = new Booking()
+    booking.seat = seat
+    booking.user_id = auth.user.id
+    booking.price = price
+    booking.table_id = table_id
+    await booking.save()
+    return response.redirect("/index")
+}
 }
 
 module.exports = TableController
