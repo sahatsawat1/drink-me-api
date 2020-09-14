5dc38f498535be94995dca84a5d4c604539e3c25
korawitice <korawit34179@gmail.com>
2020-09-15 06:39:40 +0700

    edit Usertest

--- Test/unit/Usertest.spec.js
+++ Test/unit/Usertest.spec.js
@@ -22,21 +22,22 @@ test('should return error if input incorrect data', async ({ assert }) => {
   })
   assert.isArray(validatedData.error)
 })
-test('should return only one error if input incorrect password', async ({ assert }) => {
-  const validatedData = await UserValidator({
-    frist_name: "kokozza",
-    last_name:"janama",
-    email:"iceyoyo@gmail.com",
-    password: "zaza11223344"
-  })
-  assert.equal(validatedData.error.length, 3)
-})
 test('should return error if input not unique email', async ({ assert }) => {
   const validatedData = await UserValidator({
     frist_name: "iceiceice",
     last_name:"tototo",
     email:"icerota@gmail.com",
     password: "112244556677"
+
   })
   assert.isOk(validatedData.error)
+})
+test('hould return more than one error if pass many incorrect data', async ({ assert }) => {
+  const validatedData = await UserValidator({
+    first_name: "wowza",
+    last_name: "stang",
+    email: "koarawit",
+    password: "123456789"
+  })
+  assert.isAbove(validatedData.error.length, 1)
 })
