36c9e187fd240b0373dcb94e993cdadf8a7503d2
Talinee <bieteuy@gmail.com>
2020-09-14 17:20:51 +0700

    booking #1

--- app/Models/Table.js
+++ app/Models/Table.js
@@ -3,6 +3,8 @@
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 
+class Booking extends Model {
+}
 class Table extends Model {
     static get primaryKey() {
         return "table_id";
